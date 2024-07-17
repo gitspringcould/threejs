3abe60c37f02136bb5089eacfb043a39cf97a848
webspring <1162487568@qq.com>
2024-07-17 09:22:17 +0800

    第一个threejs应用

--- apps/start/src/components/HelloWorld.vue
+++ apps/start/src/components/HelloWorld.vue
@@ -1,43 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import * as THREE from "three";
+import { onMounted } from "vue";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-defineProps({
-  msg: String,
-})
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-const count = ref(0)
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+};
+
+onMounted(() => {
+  document.getElementById("world").appendChild(renderer.domElement);
+  animate();
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div id="world"></div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+#world {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
