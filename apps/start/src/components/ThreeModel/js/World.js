ffdf147fa9214f46cde2005cf0a0551e686b2116
webspring <1162487568@qq.com>
2024-07-18 20:03:21 +0800

    加载控制器

--- apps/start/src/components/ThreeModel/js/World.js
+++ apps/start/src/components/ThreeModel/js/World.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
@@ -9,7 +10,7 @@ export default class World {
     this.scene.name = name;
     this.scene.fog = new THREE.Fog(0x000000, 0.01);
     this.scene.position.y = -2.8;
-    this.scene.add(new THREE.AmbientLight(0xffffff, 1));
+    this.scene.add(new THREE.AmbientLight(0xffffff, 10));
     this.scene.add(new THREE.DirectionalLight(0xffffff, 1));
 
     this.container = document.getElementById(selector);
@@ -25,9 +26,18 @@ export default class World {
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
     });
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+    this.controls.minDistance = 10;
+    this.controls.maxDistance = 50;
+    this.controls.maxPolarAngle = Math.PI / 2.2;
+    this.controls.update();
+
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.width, this.height);
-    this.renderer.setClearColor(0xffffff, 1);
+    this.renderer.setClearColor(0xf0f2f5, 1);
 
     this.container.appendChild(this.renderer.domElement);
 
